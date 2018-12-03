65bf75c0f30fedad7fb45b0badce41b7ceaf879d
SkyRideRq <piotrslowinski@op.pl>
2018-12-03 15:30:54 +0100

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import ToDo from './toDo'
 class App extends Component {
+  state = {
+    toDo:[
+      {id:1, content:'make game'},
+      {id:2, content:'play some game'}
+    ]
+  }
+  delToDo = (id) => {
+    const newToDo = this.state.toDo.filter(e => {
+      return e.id !== id
+    })
+    this.setState({
+      toDo: newToDo
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App container">
+        <h1 className="center blue-text">Do zrobienia</h1>
+        <ToDo toDo={this.state.toDo} delToDo={this.delToDo}/>
       </div>
     );
   }
---
+++ src/toDo.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ToDo = ({toDo, delToDo}) => {
+    const toDoList = toDo.length ? (
+        toDo.map(e => {
+            return (
+                <div className="collection-item" key={e.id}>
+                    <span onClick={()=>{delToDo(e.id)}}>{e.content}</span>
+                </div>
+            )
+        })
+    ) : (
+        <p className="center">Wszystko ogarnięte</p>
+    )
+    return (
+        <div className="toDo collection">
+            {toDoList}
+        </div>
+    )
+}
+export default ToDo
